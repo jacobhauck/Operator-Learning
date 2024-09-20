0599d742fd588d72e575c367eced50799e258c89
Jacob Hauck <hauckjp@gmail.com>
2024-09-20 16:02:58 -0500

    Document run_experiment, and add more experiments to demo.

---
+++ experiments/demo/side.py
@@ -0,0 +1,12 @@
+"""Simple demo experiment for testing run_experiment script."""
+from experiments import Experiment
+
+from .demo import _print
+
+
+class DemoSide(Experiment):
+    """Demonstration experiment that simply prints the provided config."""
+
+    def run(self, config):
+        print('Running demo SIDE Experiment.')
+        _print(config)
