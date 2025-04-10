aeb79437b0c17208f90d60e60bc1312a4f7ae982
Jacob Hauck <hauckjp@gmail.com>
2025-04-10 15:24:47 -0500

    Add operatorlearning package with added requirements, implementation of GridFunctions, and some simple tests

---
+++ operatorlearning/function.py
@@ -0,0 +1,311 @@
+import itertools
+from abc import ABC, abstractmethod
+
+import torch
+
+
+class Interpolator(ABC):
+    @abstractmethod
+    def interpolate(self, function: 'Function', x):
+        """
+        Interpolates a function to a given set of points
+        :param function: The Function object to interpolate
+        :param x: (..., d_in) the points at which to interpolate the function
+        :return: The interpolated values of the function at the given points
+        """
+        raise NotImplemented
+
+
+class Function(torch.nn.Module):
+    def __init__(self, y, x, interpolator: Interpolator):
+        """
+        Create a sampled function object
+        :param y: (..., d_out) or (...) the sampled function values
+        :param x: (..., d_in) the sampling points
+        :param interpolator: interpolator to use for evaluation
+        """
+        super(Function, self).__init__()
+
+        self.x = x
+        self.y = y
+        self.is_scalar = (len(self.y.shape) == len(self.x.shape) - 1)
+
+        assert self.y.shape[:len(self.x.shape) - 1] == self.x.shape[:-1], \
+            ('Sample values and sample points must have same shape in all but '
+             'last dimension')
+
+        self.interpolator = interpolator
+
+    @property
+    def d_in(self):
+        """Dimension of domain"""
+        return self.x.shape[-1]
+
+    @property
+    def d_out(self):
+        """Dimension of codomain"""
+        return self.y.shape[-1] if not self.is_scalar else 1
+
+    def __call__(self, x_query):
+        """
+        Evaluate the function at given points.
+        :param x_query: (..., d_in) The points at which to evaluate the function
+            (may require interpolation using the current interpolator)
+        :return: (..., d_out) or (...) The function value at the given points
+        """
+
+        assert x_query.shape[-1] == self.d_in, 'Invalid input dimension'
+
+        return self.interpolator.interpolate(self, x_query)
+
+
+def _grid_interpolate(function: 'GridFunction', x, method, extend):
+    """
+    Interpolate a GridFunction to a new set of points
+    :param function: The GridFunction to interpolate
+    :param x: (..., d_in) the set of points on which to interpolate the function
+    :param method: Method to use for interpolation. One of 'linear' or 'nearest'.
+    :param extend: How to extend the function when interpolation points are
+        outside the grid. One of 'clamped' to clamp to the nearest boundary
+        value, 'periodic' to extend periodically (this is done in a closed-open
+        manner, so the function values at the upper limits will be ignored),
+        or a given constant value to pad with.
+    """
+    assert isinstance(function, GridFunction), \
+        '`GridInterpolator` only works for `GridFunction`s'
+
+    assert function.d_in == x.shape[-1], 'Invalid input dimension'
+
+    query_shape = x.shape[:-1]
+    x = x.reshape(-1, function.d_in)  # (N, d_in)
+
+    min_tensor = torch.tensor(
+        [function.xs[i][0] for i in range(function.d_in)],
+        dtype=x.dtype, device=x.device
+    )[None]
+
+    max_tensor = torch.tensor(
+        [function.xs[i][-1] for i in range(function.d_in)],
+        dtype=x.dtype, device=x.device
+    )[None]
+
+    extend_constant = False
+    if extend == 'clamped':
+        x = torch.clamp(x, min_tensor, max_tensor)
+    elif extend == 'periodic':
+        x = (x - min_tensor) % (max_tensor - min_tensor) + min_tensor
+    else:
+        extend_constant = True
+
+    ts = []
+    left_indices = []
+    is_valid = []
+    for i in range(function.d_in):
+        left_indices_i = torch.searchsorted(function.xs[i], x[:, i].contiguous(), side='right') - 1
+        left_indices_i[x[:, i] == function.xs[i][-1]] -= 1
+        left_indices.append(left_indices_i)
+
+        if extend_constant:
+            valid = torch.bitwise_and(left_indices_i >= 0, left_indices_i < (len(function.xs[i]) - 1))
+            is_valid.append(valid)
+            left_indices_valid = left_indices_i[valid]
+            deltas_valid = function.xs[i][left_indices_valid + 1] - function.xs[i][left_indices_valid]
+
+            ts_i = torch.zeros_like(x[:, i])
+            ts_i[valid] = (x[valid, i] - function.xs[i][left_indices_valid]) / deltas_valid
+            ts.append(ts_i)
+        else:
+            deltas = function.xs[i][left_indices_i + 1] - function.xs[i][left_indices_i]
+            ts.append((x[:, i] - function.xs[i][left_indices_i]) / deltas)
+
+    is_oob = None
+    if extend_constant:
+        is_oob = torch.bitwise_not(torch.stack(is_valid)).sum(dim=0).to(bool)
+
+    if method == 'linear':
+        result = 0
+        for exponent in itertools.product(*([(0, 1)] * function.d_in)):
+            c = 1
+            for i in range(function.d_in):
+                c = ((1 - ts[i]) if exponent[i] == 0 else ts[i]) * c
+            # c has shape (N)
+
+            index = [left_indices[i] + exponent[i] for i in range(function.d_in)]
+
+            if extend_constant:
+                for i in range(function.d_in):
+                    index[i][is_oob] = 0
+
+            if not function.is_scalar:
+                index = index + [slice(None)]
+            val = function.y[*index]  # (N) or (N, d_out)
+
+            if extend_constant:
+                val[is_oob] = extend
+
+            if function.is_scalar:
+                result = c * val + result
+            else:
+                result = c[:, None] * val + result
+
+        if function.is_scalar:
+            return result.reshape(query_shape)
+        else:
+            return result.reshape(*query_shape, -1)
+
+    elif method == 'nearest':
+        shift = [ts[i] > 0.5 for i in range(function.d_in)]
+        index = [left_indices[i] + shift[i] for i in range(function.d_in)]
+
+        if extend_constant:
+            for i in range(function.d_in):
+                index[i][is_oob] = 0
+
+        if not function.is_scalar:
+            index = index + [slice(None)]
+        result = function.y[*index]  # (N) or (N, d_out)
+
+        if extend_constant:
+            result[is_oob] = extend
+
+        if function.is_scalar:
+            return result.reshape(query_shape)
+        else:
+            return result.reshape(*query_shape, -1)
+
+
+class GridInterpolator(Interpolator):
+    def __init__(self, method='linear', extend='clamped'):
+        """
+        Interpolator for functions defined on a tensor grid.
+        :param method: Interpolation method to use. One of 'nearest' or 'linear'
+        :param extend: How to handle samples that are out of bounds. One of
+            'clamped' to clamp to nearest boundary value, 'periodic' to extend
+            periodically, or a constant value, for constant extension.
+            Default: 'clamped'.
+        """
+        self.method = method
+        self.extend = extend
+
+    def interpolate(self, function: 'GridFunction', x):
+        _grid_interpolate(function, x, self.method, self.extend)
+
+
+class GridFunction(Function):
+    def __init__(self, y, x=None, interpolator: Interpolator | None = None, xs=None, is_sorted=False):
+        """
+        Create a tensor function with samples at the tensor product of the
+        given sampling points.
+        :param y: (..., d_out) or (...) the sample values
+        :param x: (x_1, ..., x_{d_in}, d_in) tensor of coordinate values. Can
+            be constructed from xs if not given (one or both of x or xs *must*
+            be given; do not give invalid combinations)
+        :param interpolator: interpolates the function. Defaults to
+            GridInterpolator
+        :param xs: List [x_1, ..., x_{d_in}] of sampling coordinates in each
+            dimension. These will be sorted for normalization (which simplifies
+            interpolation, for example). Can be inferred from x if not given.
+        :param is_sorted: If xs is given, whether it is already sorted (do not
+            lie about this!)
+        """
+
+        if x is not None:
+            if xs is None:
+                self.xs = []
+                extraction_tuple = [0] * len(x.shape)
+                for i in range(len(x.shape) - 1):
+                    extraction_tuple[i-1] = 0
+                    # noinspection PyTypeChecker
+                    extraction_tuple[i] = slice(None)
+                    extraction_tuple[-1] = i
+                    self.xs.append(x[*extraction_tuple])
+            else:
+                self.xs = xs
+
+        elif xs is not None:
+            if is_sorted:
+                self.xs = xs
+            else:
+                self.xs = [torch.sort(x_i)[0] for x_i in xs]
+
+
+            x = torch.stack(torch.meshgrid(self.xs, indexing='ij'), dim=-1)
+        else:
+            raise ValueError('Must provide one or both of x and xs')
+
+
+        if interpolator is None:
+            interpolator = GridInterpolator()
+
+        super(GridFunction, self).__init__(y, x, interpolator)
+
+    @staticmethod
+    def from_oracle(f, x=None, interpolator: Interpolator | None = None, xs=None, is_sorted=False):
+        """
+        Constructs a GridFunction from an oracle
+        :param f: The oracle, maps (..., d_in) to (..., d_out)
+        :param x: (..., d_in), optional grid sampling points
+        :param interpolator: optional interpolator
+        :param xs: list of length d_in of sampling points in each direction. At
+            least one of x or xs must be provided
+        :param is_sorted: Whether the sampling points are given in sorted order
+            (if xs is provided); points will be sorted for interpolation
+            efficiency if False is provided. Default: False.
+        """
+        if x is None:
+            if is_sorted:
+                xs = xs
+            else:
+                xs = [torch.sort(x_i)[0] for x_i in xs]
+
+
+            x = torch.stack(torch.meshgrid(xs, indexing='ij'), dim=-1)
+        else:
+            raise ValueError('Must provide one or both of x and xs')
+
+        y = f(x)
+
+        return GridFunction(y, x=x, interpolator=interpolator, xs=xs, is_sorted=True)
+
+    @staticmethod
+    def uniform_xs(min_point, max_point, num):
+        """
+        Constructs the grid points for a uniform sampling grid in each direction.
+        :param min_point: (d_in) Minimum coordinate values of input domain
+        :param max_point: (d_in) Maximum coordinate values of input domain
+        :param num: (d) or int, number of sampling points in each direction,
+            optionally one number to be used for all directions.
+        """
+        assert len(min_point) == len(max_point), \
+            'Min and max points must have same dimension'
+
+        try:
+            num = int(num)
+            num = num * torch.ones_like(min_point)
+        except ValueError:
+            assert len(num) == len(min_point), \
+                'Numbers of sampling points must have same dimension as min and max points'
+
+            num = num.to(int)
+
+        xs = []
+        for min_i, max_i, num_i in zip(min_point, max_point, num):
+            xs.append(torch.linspace(min_i, max_i, num_i + 1))
+
+        return xs
+
+    @staticmethod
+    def uniform_from_oracle(f, min_point, max_point, num):
+        """
+        Create a GridFunction on a uniform grid from an oracle.
+        :param f: The oracle, maps (..., d_in) to (..., d_out)
+        :param min_point: (d_in) The minimum coordinate values
+        :param max_point: (d_in) The maximum coordinate values
+        :param num: (d_in) or int. The number of samples in each
+            direction, optionally one number to be used for all directions
+        """
+        return GridFunction.from_oracle(
+            f,
+            xs=GridFunction.uniform_xs(min_point, max_point, num),
+            is_sorted=True
+        )
