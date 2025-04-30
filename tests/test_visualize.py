90e584d0f52a47d9349d9eb4cf0d642dbae33186
Jacob Hauck <hauckjp@gmail.com>
2025-04-30 11:37:17 -0500

    Add test_visualize script

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -1,6 +1,5 @@
 import data.external.pdebench as pdb
 
-d = pdb.PDEBenchStandardDataset('burgers', 1, nu=0.001)
+d = pdb.PDEBenchStandardDataset('reaction-diffusion', 1, nu=0.5, rho=1.0)
 f = d[2]
-f.quick_visualize(x_min=f.x_min, x_max=2*f.x_max)
-f.quick_visualize(x_min=f.x_min, x_max=f.x_max)
+f.quick_visualize()
